c1c44d73caa510d474f1e4e6abc01b45f6609852
Bolub <abiol5202@gmail.com>
2020-01-27 17:26:42 +0100

    huge huge changes

---
+++ public/Courses.js
@@ -0,0 +1,69 @@
+const Courses = [
+    {
+        "id": "basic1",
+        "Title": "Basic 1 Fashion Program",
+        "SubTitle": "From Pattern drafting, to Fashion illustration and Sketching, to Flat cutting methods, students will be exposed to all methods of cutting and presentation.",
+        "Fee": "175000",
+        "Duration": "6 months",
+        "Difficulty": "Beginner",
+        "Learn": [
+            "Trousers",
+            "Jump suits",
+            "Formal office wear",
+            "Aso ebi",
+            "All skirts",
+            "All blouses",
+            "Dart manipulation methods",
+            "Fabric manipulation methods",
+            "Professional sewing methods"
+        ],
+        "Expectation": [
+            "Students will have at least 1 graduate photoshoot and participate in fashion shows.",
+            "Students would have developed a professional portfolio and to be able to set up a standard fashion brand.",
+            "A Diploma certificate will be awarded at the end of the course"
+        ],
+        "paymentPlans": {
+            "Full": "200000",
+            "Installment": {
+                "firstPayment": "85000",
+                "secondPayment": "65000",
+                "thirdPayment": "50000"
+            }
+        }
+    },
+
+    {
+        "id": "basic2",
+        "Title": "Basic II Fashion Program",
+        "SubTitle": "From Pattern drafting, to Fashion illustration and Sketching, to Flat cutting methods, students will be exposed to all methods of cutting and presentation.",
+        "Fee": "280000",
+        "Duration": "1 year",
+        "Difficulty": "Beginner",
+        "Learn": [
+            "Trousers",
+            "Jump suits",
+            "Formal office wear",
+            "Aso ebi",
+            "All skirts",
+            "All dresses",
+            "All blouses",
+            "Dart manipulation methods",
+            "Fabric manipulation methods",
+            "Professional sewing methods"
+        ],
+        "Expectation": [
+            "Students will have at least 1 graduate photoshoot and participate in fashion shows.",
+            "Students would have developed a professional portfolio and to be able to set up a standard fashion brand.",
+            "A Diploma certificate will be awarded at the end of the course"
+        ],
+        "paymentPlans": {
+            "Full": "280000",
+            "Installment": {
+                "full": "315000",
+                "firstPayment": "175000",
+                "secondPayment": "70000",
+                "thirdPayment": "70000"
+            }
+        }
+    }
+]
